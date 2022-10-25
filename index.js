b4914f70f73869ffc3096603f41552dac270f82d
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-25 23:13:33 +0200

    FEAT: get movies by Id on movies.routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 
+const moviesRouter = require('./src/api/movies/movies.routes')
+
 const connectDb = require('./src/utils/db/db')
 
 connectDb();
@@ -11,6 +13,8 @@ const DB_URL = process.env.DB_URL;
 const server = express();
 const router = express.Router();
 
+server.use(express.json());
+server.use('/', moviesRouter)
 server.listen(PORT, () => {
     console.log(`Server running in ${PORT}`);
 })
---
+++ src/api/movies/movies.routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const Movie = require('./movies.models');
+
+const router = express.Router();
+
+
+
+router.get('/', async(req, res)=>{
+    try {
+        const allMovies = await Movie.find();
+        return res.status(200).json(allMovies);
+    } catch (error) {
+        return res.status(500).json(error)
+        
+    }
+})
+
+router.get('/:id', async(req, res)=>{
+    try {
+        const id = req.params.id;
+        const allMovies = await Movie.findById(id);
+        return res.status(200).json(allMovies);
+    } catch (error) {
+        return res.status(500).json(error)
+        
+    }
+})
+
+module.exports = router;
